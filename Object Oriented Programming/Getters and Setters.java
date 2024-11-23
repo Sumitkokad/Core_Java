26175238d5d2ce887cee6449c41ae24d9c252fb7
Sumitkokad <148705098+Sumitkokad@users.noreply.github.com>
2024-11-23 23:28:22 +0530

    Getters and Setters.java

---
+++ Object Oriented Programming/Getters and Setters.java
@@ -0,0 +1,31 @@
+/*Getters and Setters are methods used to access and modify private fields of a class. They follow the principle of encapsulation, which protects the fields from direct access.
+
+1] Getter: Retrieves (gets) the value of a private field.
+2] Setter: Updates (sets) the value of a private field. 
+*/
+
+public class Main{
+    public static void main(String args[]){
+        Pen p1=new Pen();
+        p1.setcolor("blue");
+        p1.settip(4);
+        System.out.println(p1.getcolor());
+        System.out.println(p1.gettip());
+    }
+}
+class Pen{
+   private String color;
+   private int tip;
+    String getcolor(){  //these are getters
+        return this.color;
+    }
+    int gettip(){    //these are getters
+        return this.tip;
+    }
+    void setcolor(String color){  //these are setters
+       this.color=color;
+    }
+    void settip(int tip){     //these are setters
+        this.tip=tip;
+    }
+}
