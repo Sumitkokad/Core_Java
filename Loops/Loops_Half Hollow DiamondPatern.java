9ca4e9148aa284f2a520fb5024c60ec189f95dc0
Sumitkokad <148705098+Sumitkokad@users.noreply.github.com>
2025-01-30 21:37:13 +0530

    Loops_Half Hollow DiamondPatern.java

---
+++ Loops/Loops_Half Hollow DiamondPatern.java
@@ -0,0 +1,52 @@
+/*
+
+    *
+   ***
+  *****
+ *******
+*       *
+ *     *
+  *   *
+   * *
+    *
+
+*/
+
+
+public class Main{
+    public static void main(String args[]){
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                System.out.print("*");
+            }
+            
+           System.out.println(); 
+           
+        }
+       for(int i=0;i<=n;i++){
+           for(int j=1;j<=i;j++){
+               System.out.print(" ");
+           }
+           for(int k=i;k<=(i);k++){
+               System.out.print("*");
+           }
+           
+            for(int p=i;p<n;p++){
+                System.out.print(" ");
+            }
+           
+            for(int x=i;x<n;x++){
+                if(x==3){
+                System.out.print("*");
+            }
+            else{System.out.print(" ");
+            }
+            }
+           System.out.println();
+       }
+    }
+}
